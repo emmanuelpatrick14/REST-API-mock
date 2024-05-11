4fcb6168101faa278199ee5464ac5d2611d69a9f
Emmanuel <emmanueljoeemie@gmail.com>
2024-05-11 22:30:54 +0100

    Initial commit

---
+++ config/default.ts
@@ -0,0 +1,9 @@
+// using the insralled confgi modell ...google config npm docs
+
+
+export default{
+    port: 1337,
+   
+    dbUri:
+    "mongodb+srv://emmanueljoeemie:HweJht9Zrkic8SXe@emmanueljoeemie.ogqhhdc.mongodb.net/?retryWrites=true&w=majority&appName=emmanueljoeemie"
+}
